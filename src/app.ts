4551c3aa2a3f0d16be808c82da2cb79b1d081365
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2026-09-05 20:52:42 +0600

    implement playment process ssl comerce

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,9 @@ import { notFound } from "./app/middleware/notFound";
 import { AuthRoutes } from "./app/module/auth/auth.route";
 import z from "zod";
 import { redisClient } from "./app/lib/redis";
-import crypto from "crypto"
+import crypto from "crypto";
 import { UserRoutes } from "./app/module/user/user.route";
+import { getBkashIdToken } from "./app/lib/bkash";
 
 const app: Application = express();
 
@@ -37,14 +38,8 @@ app.use("/api/v1/user", UserRoutes);
 
 app.get("/test", async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const otp = crypto.randomInt(100000,1000000);
-
-    // await redisClient.set("forget-password-otp:patient1@gmail.com", "123456", {
-    //   expiration: {
-    //     type: "EX",
-    //     value: 60,
-    //   },
-    // });
+    const grantIdTokenResult = await getBkashIdToken();
+    console.log("anything = ",grantIdTokenResult);
 
     res.status(httpStatus.OK).json({
       success: true,
